c8e31c3629e00cb74368efca7c41f7a3af63ab5e
wanghao12345 <1750110604@qq.com>
2017-12-04 17:12:47 +0800

    update

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import blog.views as bv
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', bv.index()),
 ]
