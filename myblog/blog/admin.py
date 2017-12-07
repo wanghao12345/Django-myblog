c3d86116e99c3e3b7c0ec9bb3dc623ddd88aff76
wanghao12345 <1750110604@qq.com>
2017-12-07 17:22:12 +0800

    admin

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+#from django.db import models
+from .models import Article
 
-# Register your models here.
+admin.site.register(Article)
--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=32,default='Title')
     content = models.TextField(null=True)
+    def __str__(self):
+        return self.title
