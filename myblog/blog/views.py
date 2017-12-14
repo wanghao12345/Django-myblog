483494008c71dee217819f27a387af70ae94d877
wanghao12345 <1750110604@qq.com>
2017-12-14 08:58:49 +0800

    update

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -2,8 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
 def index(request):
-    article = models.Article.objects.get(pk=1)
-    return render(request, 'blog/index.html', {"article":article})
+    # 获取所有的条数
+    articles = models.Article.objects.all()
+    return render(request,'blog/index.html',{'articles':articles})
+    # 获取某一条
+    # article = models.Article.objects.get(pk=1)
+    # return render(request, 'blog/index.html', {"article":article})
+
+def article_page(request,article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request,'blog/article_page.html',{'article':article})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
