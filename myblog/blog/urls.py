465c3edbfb8211399ee71eb01cafb1e2ab21df97
wanghao12345 <1750110604@qq.com>
2017-12-19 08:44:52 +0800

    汪浩：update

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
-
+app_name = 'blog'
 urlpatterns = [
     url('^index/$',views.index),
-    url('^article/(?P<article_id>[0-9]+)$',views.article_page)
+    url('^article/(?P<article_id>[0-9]+)$',views.article_page,name='article_page'),
+    url('^edit/$',views.edit_page,name='edit_page'),
+    url('^edit/action$',views.edit_action,name='edit_action')
 ]
--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls',namespace='blog')),
 ]
